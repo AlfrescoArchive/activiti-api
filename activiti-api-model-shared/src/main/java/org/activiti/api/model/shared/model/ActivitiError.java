58d7a0ec14a2ca8041b3f1d22e32e1c4b6f16bb2
Miguel Ruiz <miguel.ruiz.developer@gmail.com>
2019-04-10 13:56:43 +0100

    Change HttpStatus for int type

--- activiti-api-model-shared/src/main/java/org/activiti/api/model/shared/model/ActivitiError.java
+++ activiti-api-model-shared/src/main/java/org/activiti/api/model/shared/model/ActivitiError.java
@@ -1,10 +1,8 @@
 package org.activiti.api.model.shared.model;
 
-import org.springframework.http.HttpStatus;
-
 public interface ActivitiError {
 
-    HttpStatus getStatus();
+    int getStatus();
 
     String getMessage();
 
