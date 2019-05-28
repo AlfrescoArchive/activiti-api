53b832c3e4e9ba7f37647a8f599575255ac179c5
Igor Dianov <igor@dianov.com>
2019-05-27 19:02:07 -0700

    feat: add SecurityManager.getAuthenticatedUserGroups method

--- activiti-api-runtime-shared/src/main/java/org/activiti/api/runtime/shared/security/SecurityManager.java
+++ activiti-api-runtime-shared/src/main/java/org/activiti/api/runtime/shared/security/SecurityManager.java
@@ -1,7 +1,14 @@
 package org.activiti.api.runtime.shared.security;
 
+import java.util.Collections;
+import java.util.List;
+
 public interface SecurityManager {
 
     String getAuthenticatedUserId();
+    
+    default List<String> getAuthenticatedUserGroups() {
+        return Collections.emptyList();
+    }
 
 }
