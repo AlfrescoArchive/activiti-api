6b3ed43dbb1471655743768d577d3ac016e4fb41
Igor Dianov <igor@dianov.com>
2019-08-01 07:15:37 -0700

    refactor: extended SecurityManager interface methods

--- activiti-api-runtime-shared/src/main/java/org/activiti/api/runtime/shared/security/SecurityManager.java
+++ activiti-api-runtime-shared/src/main/java/org/activiti/api/runtime/shared/security/SecurityManager.java
@@ -1,14 +1,33 @@
 package org.activiti.api.runtime.shared.security;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.Collection;
 
 public interface SecurityManager {
 
+    /**
+     * Get currently authenticated user id from application security context  
+     * 
+     * @return currently authenticate user id or empty string if anonymous user
+     * 
+     */
     String getAuthenticatedUserId();
     
-    default List<String> getAuthenticatedUserGroups() {
-        return Collections.emptyList();
-    }
+    /**
+     * Get currently authenticated user group names from application security context 
+     * 
+     * @return list of group names the current user is member of
+     * 
+     * @throws SecurityException if principal security context does not exists.
+     */
+    Collection<String> getAuthenticatedUserGroups() throws SecurityException;
+    
+    /**
+     * Get currently authenticated user list of role names from application security context 
+     * 
+     * @return list of roles names or empty collection
+     * 
+     * @throws SecurityException if principal security context does not exists.
+     */
+    Collection<String> getAuthenticatedUserRoles() throws SecurityException;
 
 }
